43d4c5a3739fe6e8337a9d756e436f44dcaf5bf1
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-09-04 08:25:08 +0300

    feat: add color conversion

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param value - current color value
+ * @param targetSystem - if student can do programming and know basics
+ * @param config - private student ability to perform for different focus modes
+ * @returns string with value for color in target color system
+ */
+module.exports = function convertColor(
+    value = "",
+    targetSystem = "rgb"
+) {
+    return '';
+};
+  
