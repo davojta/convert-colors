1a006ec9eab7f204089a60189369f477c08c7fd1
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-09-04 08:25:33 +0300

    feat: add tests

--- index.test.js
+++ index.test.js
@@ -1,55 +1,14 @@
 var { expect } = require('chai')
 
-const getTimeForEducation = require('./src/index')
-
-const defaultStudentSpeedConfig = {
-    family: 4,
-    friends: 10,
-    normal_life: 20,
-    profession: 30,
-    carrier: 40,
-    top_peformance: 60
-}
-
-const talentedStudentSpeedConfig = {
-    normal_life: 40,
-    profession: 50,
-    carrier: 60,
-    top_peformance: 80
-}
+const convertColor = require('./src/index')
 
 
-describe('js-edu', () => {
-    describe('for general student with programming skills', () => {
-        it('should could take 200 weeks for family focus', () => {
-            const weeks = getTimeForEducation('family', true,  defaultStudentSpeedConfig);
-    
-            expect(weeks).to.equal(200);
+describe('convert-colors', () => {
+    describe('from hex to rgb', () => {
+        it('#000000 to rbg(0,0,0)', () => {
+            const res = convertColor('#000000');
+
+            expect(res).to.equal('rgb(0,0,0)')
         });
-        it('should could take 80 weeks for friends focus', () => {
-            const weeks = getTimeForEducation('friends', true,  defaultStudentSpeedConfig);
-    
-            expect(weeks).to.equal(80);
-        });
-        it('should could take 40 weeks for normal_life focus', () => {
-            const weeks = getTimeForEducation('normal_life', true,  defaultStudentSpeedConfig);
-    
-            expect(weeks).to.equal(40);
-        });
-        it('should could take 27 weeks for profession focus', () => {
-            const weeks = getTimeForEducation('profession', true,  defaultStudentSpeedConfig);
-    
-            expect(weeks).to.equal(27);
-        });
-        it('should could take 20 weeks for carrier focus', () => {
-            const weeks = getTimeForEducation('carrier', true,  defaultStudentSpeedConfig);
-    
-            expect(weeks).to.equal(20);
-        });
-        it('should could take 14 weeks for top_peformance focus', () => {
-            const weeks = getTimeForEducation('top_peformance', true,  defaultStudentSpeedConfig);
-    
-            expect(weeks).to.equal(14);
-        });
-    })
+    });
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /**
  * @param value - current color value
  * @param targetSystem - if student can do programming and know basics
- * @param config - private student ability to perform for different focus modes
  * @returns string with value for color in target color system
  */
 module.exports = function convertColor(
